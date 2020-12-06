d4ad73e7492ceb662ebff5aded3d39adca433fc0
Gaicean23 <gaychanvova60115@mail.ru>
2020-12-06 14:06:15 +0200

    обн

--- assets/js/get.js
+++ assets/js/get.js
@@ -1,26 +1,26 @@
 // //Запросы от сервера
 
-// async function getData(url = '') {
-//   const response = await fetch(url, {
-//     method: 'GET',
-//     headers: {
-//       'Content-type': 'application/json'
-//     }
-//   }); 
+async function getData(url = '') {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }); 
 
-//   if(!response.ok) throw Error(response.status);
+  if(!response.ok) throw Error(response.status);
 
-//   return response.json();
-// };
-
-
+  return response.json();
+};
 
 
 
-// //https://jsonplaceholder.typicode.com/posts
 
-// //https://jsonplaceholder.typicode.com/posts/1
 
-// getData('https://jsonplaceholder.typicode.com/posts')
-// .then((data) => console.log(data))
-// .catch((err) => console.log(err));
+//https://jsonplaceholder.typicode.com/posts
+
+//https://jsonplaceholder.typicode.com/posts/1
+
+getData('https://jsonplaceholder.typicode.com/posts')
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
