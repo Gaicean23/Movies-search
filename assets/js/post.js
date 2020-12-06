e46e14093faa3b9dc42a1bbfb856b407fa2020fa
Gaicean23 <gaychanvova60115@mail.ru>
2020-12-06 14:20:59 +0200

    /

--- assets/js/post.js
+++ assets/js/post.js
@@ -3,27 +3,27 @@
 
 
 
-async function getData(url = '', data = {}) {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }); 
+// async function getData(url = '', data = {}) {
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   }); 
 
-  if(!response.ok) throw Error(response.status);
+//   if(!response.ok) throw Error(response.status);
 
-  return response.json();
-};
+//   return response.json();
+// };
 
 
-//https://jsonplaceholder.typicode.com/posts
+// //https://jsonplaceholder.typicode.com/posts
 
-getData('https://jsonplaceholder.typicode.com/posts', {
-title: 'My custom title',  
-body: 'My custom body text',
-userId: 100
-})
-.then((data) => console.log(data))
-.catch((err) => console.log(err));
+// getData('https://jsonplaceholder.typicode.com/posts', {
+// title: 'My custom title',  
+// body: 'My custom body text',
+// userId: 100
+// })
+// .then((data) => console.log(data))
+// .catch((err) => console.log(err));
--- assets/js/put.js
+++ assets/js/put.js
@@ -1,27 +1,27 @@
 //обновления 
 
 
-async function getData(url = '', data = {}) {
-  const response = await fetch(url, {
-    method: 'PUT',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }); 
+// async function getData(url = '', data = {}) {
+//   const response = await fetch(url, {
+//     method: 'PUT',
+//     headers: {
+//       'Content-type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   }); 
 
-  if(!response.ok) throw Error(response.status);
+//   if(!response.ok) throw Error(response.status);
 
-  return response.json();
-};
+//   return response.json();
+// };
 
 
-//https://jsonplaceholder.typicode.com/posts
+// //https://jsonplaceholder.typicode.com/posts
 
-getData('https://jsonplaceholder.typicode.com/posts/88', {
-title: 'My custom title',  
-body: 'My custom body text',
-userId: 1
-})
-.then((data) => console.log(data))
-.catch((err) => console.log(err));
+// getData('https://jsonplaceholder.typicode.com/posts/88', {
+// title: 'My custom title',  
+// body: 'My custom body text',
+// userId: 1
+// })
+// .then((data) => console.log(data))
+// .catch((err) => console.log(err));
